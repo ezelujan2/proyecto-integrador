9057c4d7d3f4420eed4f2fd196e7ee0dd8fc4a6e
ezelujan2 <eze.rosario2000@gmail.com>
2023-07-17 14:11:20 -0300

    getCharbyId cambiado a async function

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -2,13 +2,11 @@ const URL = "https://rickandmortyapi.com/api/character/";
 const axios = require("axios")
 
 
-function routerCharacter (req, res) {
-    axios(URL + req.params.id)
-    .then(response => {
-        const data = response.data;
-        if(!data.id) res.send('El personaje no fue encontrado')
-        else {
-        const personaje = {
+async function  routerCharacter (req, res) {
+    try {
+    const {data}  = await axios(URL + req.params.id)
+    if(!data) throw new Error('Personaje no encontrado')
+    const personaje = {
             id : data.id,
             status: data.status,
             name: data.name,
@@ -17,10 +15,13 @@ function routerCharacter (req, res) {
             image: data.image,
             gender: data.gender,
         }
-        res.json(personaje)
-    }})
-    .catch(error => {
-        if(error.message.includes('404')) res.status(404).send('Not found')
-        else res.status(500).send(error.message)})
+    res.json(personaje)
+    
+        
+    } catch (error) {
+        console.log(error)
+        if(error.message.includes('404')) res.status(404).send('Not found');
+        else res.status(500).send(error.message)
+    }
 }
 module.exports = routerCharacter;
