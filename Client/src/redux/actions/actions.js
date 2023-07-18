608a9a1689fdb4b1ed43b07247cdd21674969a8b
ezelujan2 <eze.rosario2000@gmail.com>
2023-07-18 09:36:38 -0300

    Agregado manejo de errores en actions, corrección de syntax error en reducer

--- Client/src/redux/actions/actions.js
+++ Client/src/redux/actions/actions.js
@@ -5,11 +5,17 @@ export const addFav = (character) => {
     const endpoint = 'http://localhost:3001/rickandmorty/fav';
 
     return async (dispatch) => {
-      const {data} = await axios.post(endpoint,character);
+      try {
+         const {data} = await axios.post(endpoint,character);
+         if(!data.length) throw Error('No hay favoritos')
       return dispatch({
          type: ADD_FAV,
          payload: data,
       })
+      } catch (error) {
+         console.log(error.message)
+      }
+      
    }
  };
  export const removeFav =  (id) => {
